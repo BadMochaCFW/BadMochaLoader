d35050665e2600887e5f0ca0261145d6078c50fb
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2023-03-14 23:21:21 -0400

    Update main.c

--- arm/main.c
+++ arm/main.c
@@ -52,37 +52,19 @@ void NORETURN _main(void* base) {
 	irq_initialize();
 	printf("[ OK ] Setup Interrupts\n");
 
-	srand(read32(LT_TIMER));
-	crypto_initialize();
-	printf("[ OK ] Setup Crypto\n");
-
-	nand_initialize();
-	printf("[ OK ] Setup NAND\n");
-
-	sdcard_init();
-	printf("[ OK ] Setup SD Card\n");
+	);
 
 	int res = ELM_Mount();
-	if (res) {
-		char errorstr[] = "Couldn't mount SD card! See Gamepad for details.";
+
+		char errorstr[] = "Hello World";
 		gfx_draw_string(GFX_TV, errorstr, (1280 - sizeof(errorstr)*8) / 2, 500, WHITE);
 		printf("[FATL] SD Card mount error: %d\n", res);
-		panic(0);
-	}
-	printf("[ OK ] Mounted SD Card\n");
-
-	isfs_init();
-	printf("[ OK ] Mounted SLC\n");
-
-	write32(LT_AHBPROT, 0xFFFFFFFF);
-	write32(LT_GPIO_ENABLE, 0xFFFFFFFF);
-	write32(LT_GPIO_OWNER, 0xFFFFFFFF);
-	printf("[ OK ] Unrestricted Hardware\n");
+);
 
 	printf("--------------------------\n");
 	printf("          Ready!          \n");
 	printf("--------------------------\n");
-	//We're good to go!
+	
 
-	app_run();
+
 }
